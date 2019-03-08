2a9292c69364d666d246cf6b5bdc111297c56e94
Chiarelli Thomas <45951053+Mutsarano3@users.noreply.github.com>
2019-03-08 19:56:55 +0100

    Arduino_Code_UltraSon

---
+++ Ultrason.ino
@@ -0,0 +1,30 @@
+
+const byte TRIGGER_PIN = 12; // Broche TRIGGER
+const byte ECHO_PIN = 2;    // Broche ECHO
+const unsigned long MEASURE_TIMEOUT = 25000UL; // 25ms = ~8m à 340m/s
+const float SOUND_SPEED = 340.0 / 1000;
+void setup() {
+  Serial.begin(115200);
+  pinMode(TRIGGER_PIN, OUTPUT);
+  digitalWrite(TRIGGER_PIN, LOW); // La broche TRIGGER doit être à LOW au repos
+  pinMode(ECHO_PIN, INPUT);
+}
+
+void loop() {
+  
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER_PIN, LOW);
+  long measure = pulseIn(ECHO_PIN, HIGH, MEASURE_TIMEOUT);
+  float distance_mm = measure / 2.0 * SOUND_SPEED;
+  Serial.print(F("Distance: "));
+  Serial.print(distance_mm);
+  Serial.print(F("mm ("));
+  Serial.print(distance_mm / 10.0, 2);
+  Serial.print(F("cm, "));
+  Serial.print(distance_mm / 1000.0, 2);
+  Serial.println(F("m)"));
+  delay(500);
+}
+
+
